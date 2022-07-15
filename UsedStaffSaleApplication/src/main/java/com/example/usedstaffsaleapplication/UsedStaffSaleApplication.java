49a2dafa496f2f50f8dbd6dbe7d62d0aa217c6a3
malizorba <97551928+malizorba@users.noreply.github.com>
2022-07-15 17:56:52 +0300

    Used Staff Sale Application

---
+++ UsedStaffSaleApplication/src/main/java/com/example/usedstaffsaleapplication/UsedStaffSaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.usedstaffsaleapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedStaffSaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsedStaffSaleApplication.class, args);
+    }
+
+}
